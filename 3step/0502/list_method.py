2a7a9a512d7c684e2ecd50341e9d16a9dcd086c4
”tanakatatsuya” <tanaka@gmail.com>
2020-07-08 16:22:13 +0900

    リストに紐付いた関数を呼び出す

---
+++ 3step/0502/list_method.py
@@ -0,0 +1,7 @@
+names = []
+names.append("山田太郎")
+names.append("佐藤次郎")
+names.append("鈴木花子")
+names.append("小川裕子")
+names.insert(3, "井上健太")
+print(names)
---
+++ 3step/0502/list_method2.py
@@ -0,0 +1,5 @@
+names = ["山田太郎", "佐藤次郎", "鈴木花子", "井上健太", "小川裕子"]
+
+print(names.pop(3))
+names.remove("小川裕子")
+print(names)
