346b4ce8b9705be0e7a8c27a7b6651dce5e3e60c
”tanakatatsuya” <tanaka@gmail.com>
2020-07-10 00:22:01 +0900

    リストや辞書から順に値を取り出す

---
+++ 3step/0702/for.py
@@ -0,0 +1,10 @@
+messages = [
+    "頭隠して尻隠さず",
+    "犬も歩けば房にあたる",
+    "瓜の蔓に茄子はならぬ",
+    "江戸の仇を長崎で討つ",
+    "鬼の居ぬ間に洗濯",
+]
+
+for message in messages:
+    print(message)
---
+++ 3step/0702/for_dic.py
@@ -0,0 +1,8 @@
+addresses = {
+    "名無権兵衛":"千葉県千葉市美浜町1-1-1",
+    "山田太郎":"東京都練馬区蔵王町2-2-2",
+    "鈴木花子":"埼玉県所沢市大竹町3-3-3",
+}
+
+for key, value in addresses.items():
+    print(key,":" ,value)
