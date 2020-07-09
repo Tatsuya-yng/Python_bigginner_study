975a688456b4a97759cf62eca888429407235630
”tanakatatsuya” <tanaka@gmail.com>
2020-07-09 19:10:14 +0900

    より複雑な分岐を試す

---
+++ 3step/0603/if.py
@@ -0,0 +1,10 @@
+point = int(input("あなたの点数は？"))
+
+if point >= 90:
+    print("素晴らしい！文句なしの合格です!")
+elif point >= 70:
+    print("おめでとう!合格です!")
+elif point >= 50:
+    print("残念!もう少しでした・・・・・")
+else:
+    print("残念・・・不合格・・・・・")
