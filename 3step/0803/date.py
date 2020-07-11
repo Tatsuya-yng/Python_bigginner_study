9f36ef0233e1f8bc4ccf0bbc74705e1b749ae84f
”tanakatatsuya” <tanaka@gmail.com>
2020-07-11 18:28:50 +0900

    日付/時刻を操作する

---
+++ 3step/0803/date.py
@@ -0,0 +1,13 @@
+import datetime
+
+today = datetime.date.today()
+print("今日は", today, "です。")
+birth = datetime.date(today.year, 6, 25)
+ellap = birth - today
+if ellap.days == 0:
+    print("今日は誕生日です！")
+elif ellap.days > 0:
+    print("今年の誕生日まで、あと", ellap.days, "日です。")
+else:
+    print("今年の誕生日は、", -ellap.days, "日、過ぎました。")
+    
