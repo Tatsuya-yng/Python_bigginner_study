15fabdcda59a63427f751f7c5a9e380fb24daf34
”tanakatatsuya” <tanaka@gmail.com>
2020-07-13 03:28:53 +0900

    基本的なクラスを理解する

---
+++ 3step/1001/class_client.py
@@ -0,0 +1,9 @@
+import myclass
+
+p1 = myclass.Person("サクラ", 1.21, 23)
+bmi1 = p1.weight / (p1.height * p1.height)
+print(p1.name, "のBMI値は", bmi1, "です。")
+
+p2 = myclass.Person("キラ", 1.35, 30)
+bmi2 = p2.weight / (p2.height * p2.height)
+print(p2.name, "のBMI値は", bmi2, "です。")
